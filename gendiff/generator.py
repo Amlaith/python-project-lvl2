faf4d572b2074f3f3ff56507a7190c1e6eced45a
Amlaith <burjooi@gmail.com>
2020-05-05 22:59:04 +0300

    codeclimate experiments #2

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -9,23 +9,35 @@ def generate_diff(path1, path2):
     old = json.load(open(path1))
     new = json.load(open(path2))
 
-    result = ['{']
+    result = [to_string(k, v) if
+        k in new and v == new[k] else
+        to_string(k, v, '-') + '\n' + to_string(k, new[k], '+')
+        if
+            k in new else
+            to_string(k, v, '-')
+        for k, v in old.items()]
+
+    result = result + [to_string(k, v, '+') for k, v in new.items() if k not in old]
+
+    result = ['{'] + result + ['}']
+
+    # result = ['{']
 
     # new_keys = set(new.keys()) - set(old.keys())
 
-    for key, value in old.items():
-        if key in new and value == new[key]:
-            result.append(to_string(key, value))
-        elif key in new:
-            result.append(to_string(key, value, '-'))
-            result.append(to_string(key, new[key], '+'))
-        else:
-            result.append(to_string(key, value, '-'))
+    # for key, value in old.items():
+    #     if key in new and value == new[key]:
+    #         result.append(to_string(key, value))
+    #     elif key in new:
+    #         result.append(to_string(key, value, '-'))
+    #         result.append(to_string(key, new[key], '+'))
+    #     else:
+    #         result.append(to_string(key, value, '-'))
+    #
+    # for key, value in new.items():
+    #     if key not in old:
+    #         result.append(to_string(key, value, '+'))
 
-    for key, value in new.items():
-        if key not in old:
-            result.append(to_string(key, value, '+'))
-
-    result.append('}')
+    # result.append('}')
 
     return '\n'.join(result)
